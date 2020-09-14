8adec489f202188a4f49c845dd7c323988e1e15e
syteo <shin.shiying@gmail.com>
2020-09-15 05:33:41 +0800

    firebase.js

--- admin-openhouse/src/App.js
+++ admin-openhouse/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-
-const firebase = require('firebase');
+import firebase from './firebase';
 
 class App extends React.Component {
   constructor() {
