66e842613d2f1908130c883a65ebe4bfe834b98b
beatricia <beatricia.inspirit@gmail.com>
2020-10-13 03:23:21 +0800

    Navbar, Footer

--- admin-openhouse/src/components/Navbar.js
+++ admin-openhouse/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
 import "../css/NavBar.css";
 import WebAppLogo from "../img/WebAppLogo.png";
@@ -9,11 +9,22 @@ export default class NavBar extends React.Component {
     render(){
         return (
             <div>    
-                <Navbar id="navbar">
-                    <Navbar.Brand>
+                <Navbar id="navbar" fixed="top">
+                    <Navbar.Brand href="/SAHome" id="webAppLogoNav">
                         <img src={WebAppLogo} id="webAppLogo" />
                     </Navbar.Brand>
+                    
+                    <Nav id="navContent" className="justify-content-end">
+                        <Nav.Item>
+                            <Nav.Link id="SAEmail" className="text-center">super admin email</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="/Login" id="logoutLink" className="text-center">Logout</Nav.Link>
+                        </Nav.Item>
+                        
+                    </Nav>
                 </Navbar>
+
             </div>
         )
     }
--- admin-openhouse/src/pages/Super_Administrator/SAHome.js
+++ admin-openhouse/src/pages/Super_Administrator/SAHome.js
@@ -2,8 +2,11 @@ import React, { Component } from "react";
 import fire from "../../config/firebase";
 import history from "../../config/history";
 import firecreate from "../../config/firebasecreate";
+import { Container } from 'react-bootstrap';
 
+import "../../css/SAHome.css";
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 
 class SAHome extends Component {
@@ -123,79 +126,86 @@ class SAHome extends Component {
 
   render() {
     return (
-      <div className="home">
-        <Navbar />
+      <div className="home SAHomeCon">
+        <Container className="SAHomeCon">
+          <Navbar />
 
-        <div>
-          <table class="table table-bordered">
-            <tbody>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Type of User</th>
-              </tr>
-              {this.state.users &&
-                this.state.users.map((user) => {
-                  return (
-                    <tr>
-                      <td>{user.id} </td>
+          <div>
+            <table class="table table-bordered">
+              <tbody>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Type of User</th>
+                </tr>
+                
+                {this.state.users &&
+                  this.state.users.map((user) => {
+                    return (
+                      <tr>
+                        <td>{user.id} </td>
 
-                      <td>{user.name} </td>
-                      <td>{user.email} </td>
-                      <td>{user.administratorType} </td>
-                      <td>
-                        <button
-                          onClick={(e) => {
-                            this.DeleteUser(e, user.id);
-                          }}
-                        >
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-        </div>
-        <form onSubmit={this.addUser}>
-          <input
-            type="text"
-            name="fullname"
-            placeholder="Full name"
-            onChange={this.updateInput}
-            value={this.state.fullname}
-            required
-          />
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            onChange={this.updateInput}
-            value={this.state.email}
-            required
-          />
-          <input
-            type="text"
-            name="administratorType"
-            placeholder="Type of User"
-            onChange={this.updateInput}
-            value={this.state.administratorType}
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={this.updateInput}
-            value={this.state.password}
-            required
-          />
-          <button type="submit">Add User</button>
-        </form>
-        <button onClick={this.changepasswordpage}>Change Password</button>
-        <button onClick={this.logout}>Logout</button>
+                        <td>{user.name} </td>
+                        <td>{user.email} </td>
+                        <td>{user.administratorType} </td>
+                        <td>
+                          <button
+                            onClick={(e) => {
+                              this.DeleteUser(e, user.id);
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+
+            <form onSubmit={this.addUser}>
+              <input
+                type="text"
+                name="fullname"
+                placeholder="Full name"
+                onChange={this.updateInput}
+                value={this.state.fullname}
+                required
+              />
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                onChange={this.updateInput}
+                value={this.state.email}
+                required
+              />
+              <input
+                type="text"
+                name="administratorType"
+                placeholder="Type of User"
+                onChange={this.updateInput}
+                value={this.state.administratorType}
+                required
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={this.updateInput}
+                value={this.state.password}
+                required
+              />
+              <button type="submit">Add User</button>
+            </form>
+            <button onClick={this.changepasswordpage}>Change Password</button>
+            <button onClick={this.logout}>Logout</button>
+
+          </div>
+          
+          <Footer />
+        </Container>
       </div>
     );
   }
