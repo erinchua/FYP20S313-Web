b8b47c682ad2664fd42e491e1f6d1d7639fc899f
Erin Chua <erinenin@gmail.com>
2020-10-16 12:33:30 +0800

    Created SideNavBar files

---
+++ admin-openhouse/src/components/SideNavbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+
+import '../css/SideNavBar.css';
+
+export default class SideNavBar extends React.Component {
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
--- admin-openhouse/src/pages/Marketing_Administrator/MAHome.js
+++ admin-openhouse/src/pages/Marketing_Administrator/MAHome.js
@@ -1,15 +1,30 @@
-import {  } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import React, { Component } from "react";
 import fire from "../../config/firebase";
 import history from "../../config/history";
 
 import '../../css/Marketing_Administrator/MAHome.css';
+import NavBar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 class MAHome extends Component {
 
     render() {
         return (
-            <div>hello</div>
+            <div>
+                <Container fluid className="MAHome-container">
+                    <NavBar />
+
+                    <Container fluid className="MAHome-content">
+                        
+
+
+                    </Container>
+
+                    <Footer />
+
+                </Container>
+            </div>
         )
     }
 
