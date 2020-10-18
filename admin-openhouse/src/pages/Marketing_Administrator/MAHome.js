a3e04637ada585a6e2902c401aaf0a7f388c032f
Erin Chua <erinenin@gmail.com>
2020-10-18 23:37:04 +0800

    Edited SideNavBar

--- admin-openhouse/src/pages/Marketing_Administrator/MAHome.js
+++ admin-openhouse/src/pages/Marketing_Administrator/MAHome.js
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import React, { Component } from "react";
 import fire from "../../config/firebase";
 import history from "../../config/history";
@@ -17,13 +17,24 @@ class MAHome extends Component {
                     <NavBar isMA={true} />
 
                     <Container fluid className="MAHome-content">
-                        <SideNavBar />
 
+                        <Container fluid className="MAHome-content">
+                            <Row>
+                                
+                                <Col md={2}>
+                                    <SideNavBar />
+                                </Col>
+
+                                <Col md={10}>
+                                    <p>Hello</p>
+                                </Col>
+
+                            </Row>
+                        </Container>
 
                     </Container>
 
                     <Footer />
-
                 </Container>
             </div>
         )
