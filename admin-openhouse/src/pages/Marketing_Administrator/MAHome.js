fa7c59c1156599e9e4cc6d1e2a9036f1e83ff551
Erin Chua <erinenin@gmail.com>
2020-10-19 00:54:34 +0800

    .

--- admin-openhouse/src/pages/Marketing_Administrator/MAHome.js
+++ admin-openhouse/src/pages/Marketing_Administrator/MAHome.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import React, { Component } from "react";
 import fire from "../../config/firebase";
 import history from "../../config/history";
@@ -7,6 +7,8 @@ import '../../css/Marketing_Administrator/MAHome.css';
 import NavBar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import SideNavBar from '../../components/SideNavbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
 class MAHome extends Component {
 
@@ -20,13 +22,21 @@ class MAHome extends Component {
 
                         <Container fluid className="MAHome-content">
                             <Row>
-                                
+
                                 <Col md={2}>
                                     <SideNavBar />
                                 </Col>
 
-                                <Col md={10}>
-                                    <p>Hello</p>
+                                <Col md={10} id="MAHome-topContentContainer">
+                                    <Row id="MAHome-firstRow"></Row>
+                                    <Row>
+                                        <Col md={10} id="MAHome-secondRowCol1">
+                                            <p>Real-Time Numbers</p>
+                                        </Col>
+                                        <Col md={2} id="MAHome-secondRowCol2">
+                                            <Button size="sm" id="MAHome-refreshBtn"><FontAwesomeIcon size="lg" icon={faSyncAlt}/></Button>
+                                        </Col>
+                                    </Row>
                                 </Col>
 
                             </Row>
