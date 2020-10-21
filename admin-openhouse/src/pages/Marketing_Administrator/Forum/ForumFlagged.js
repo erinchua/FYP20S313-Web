47d0cc12107468c47940309674a6acae4042b11d
Erin Chua <erinenin@gmail.com>
2020-10-21 17:33:32 +0800

    Done Flagged Forum page with Remove/Keep Modals

--- admin-openhouse/src/pages/Marketing_Administrator/Forum/ForumFlagged.js
+++ admin-openhouse/src/pages/Marketing_Administrator/Forum/ForumFlagged.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Table, Nav } from 'react-bootstrap';
+import { Container, Row, Col, Table, Button, Modal } from 'react-bootstrap';
 import React, { Component } from "react";
 import fire from "../../../config/firebase";
 import history from "../../../config/history";
@@ -7,8 +7,104 @@ import '../../../css/Marketing_Administrator/Forum.css';
 import NavBar from '../../../components/Navbar';
 import Footer from '../../../components/Footer';
 import SideNavBar from '../../../components/SideNavbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 class ForumFlagged extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            removeModal: false,
+            keepModal: false,
+        };
+    }
+
+    //Remove Post Modal
+    handleRemoveModal = () => {
+        this.removeModal = this.state.removeModal;
+        if (this.removeModal == false) {
+            this.setState({
+                removeModal: true
+            });
+        } else {
+            this.setState({
+                removeModal: false
+            });
+        }
+    }
+
+    //Keep Post Modal
+    handleKeepModal = () => {
+        this.keepModal = this.state.keepModal;
+        if (this.keepModal == false) {
+            this.setState({
+                keepModal: true
+            });
+        } else {
+            this.setState({
+                keepModal: false
+            });
+        }
+    }
+
+    //Remove Post Modal - Cancel Button
+    handleRemoveCancel = () => {
+        this.removeModal = this.state.removeModal;
+        if (this.removeModal == true) {
+            this.setState({
+                removeModal: false
+            });
+        } else {
+            this.setState({
+                removeModal: true
+            });
+        }
+    }
+
+    //Keep Post Modal - Cancel Button
+    handleKeepCancel = () => {
+        this.keepModal = this.state.keepModal;
+        if (this.keepModal == true) {
+            this.setState({
+                keepModal: false
+            });
+        } else {
+            this.setState({
+                keepModal: true
+            });
+        }
+    }
+
+    //Remove Post Modal - Confirm Button
+    handleRemoveConfirm = () => {
+        console.log("Confirm remove post")
+        this.removeModal = this.state.removeModal;
+        if (this.removeModal == true) {
+            this.setState({
+                removeModal: false
+            });
+        } else {
+            this.setState({
+                removeModal: true
+            });
+        }
+    }
+
+    //Keep Post Modal - Cancel Button
+    handleKeepConfirm = () => {
+        console.log("Confirm keep post")
+        this.keepModal = this.state.keepModal;
+        if (this.keepModal == true) {
+            this.setState({
+                keepModal: false
+            });
+        } else {
+            this.setState({
+                keepModal: true
+            });
+        }
+    }
 
     render() {
         return (
@@ -35,27 +131,28 @@ class ForumFlagged extends Component {
                                                 <Table responsive="sm" bordered id="Forum-tableContainer">
                                                     <thead id="Forum-tableHeader">
                                                         <tr>
-                                                            <th>S/N</th>
-                                                            <th>Question</th>
+                                                            <th id="ForumFlagged-SNo">S/N</th>
+                                                            <th id="ForumFlagged-questionsComments">Question/Comment</th>
+                                                            <th id="ForumFlagged-reasons">Reason</th>
+                                                            <th>Type</th>
                                                             <th>Posted By</th>
                                                             <th>Date/Time</th>
-                                                            <th>No. of Comments</th>
+                                                            <th id="ForumFlagged-comments">No. of Comments</th>
+                                                            <th id="ForumFlagged-icons">Remove Post</th>
+                                                            <th id="ForumFlagged-icons">Keep Post</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody id="Forum-tableBody">
                                                         <tr>
                                                             <td>1</td>
-                                                            <td className="Forum-question"><a href="" className="Forum-questionLink">Anyone going for the Digital Systems Security (University of Wollongong) programme talk?</a></td>
+                                                            <td>Nah I don't want</td>
+                                                            <td>This comment seems kind of rude.</td>
+                                                            <td>Comment</td>
                                                             <td>Martin John</td>
-                                                            <td>20th November 2020, 8.55pm</td>
+                                                            <td>21st November 2020, 8.35am</td>
                                                             <td>0</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>2</td>
-                                                            <td className="Forum-question"><a href="" className="Forum-questionLink">Where's the ATM?</a></td>
-                                                            <td>John Tan</td>
-                                                            <td>21st November 2020, 10.00am</td>
-                                                            <td>1</td>
+                                                            <td><Button size="lg" id="ForumFlagged-removeBtn" onClick={this.handleRemoveModal.bind(this)}><FontAwesomeIcon size="lg" icon={faTimesCircle}/></Button></td>
+                                                            <td><Button size="lg" id="ForumFlagged-keepBtn" onClick={this.handleKeepModal.bind(this)}><FontAwesomeIcon size="lg" icon={faCheckCircle}/></Button></td>
                                                         </tr>
                                                     </tbody>
                                                 </Table>
@@ -69,6 +166,63 @@ class ForumFlagged extends Component {
 
                     <Footer />
                 </Container>
+
+                {this.state.removeModal == true ? 
+                    <Modal show={this.state.removeModal} onHide={this.handleRemoveModal} size="md" centered keyboard={false}>
+                        <Modal.Header closeButton></Modal.Header>
+                        <Modal.Body>
+                            <Row className="justify-content-center">
+                                <Col md={12} className="text-center ForumFlagged-removeModalCol">
+                                    <FontAwesomeIcon size="3x" icon={faTimesCircle}/>
+                                </Col>
+                            </Row>
+
+                            <Row className="justify-content-center">
+                                <Col md={12} className="text-center ForumFlagged-removeModalCol">
+                                    <h5 id="ForumFlagged-removeText">Do you want to delete this post?</h5>
+                                </Col>
+                            </Row>
+
+                            <Row className="justify-content-center">
+                                <Col md={6} className="text-right ForumFlagged-removeModalCol">
+                                    <Button id="ForumFlagged-removeConfirmBtn" onClick={this.handleRemoveConfirm}>Confirm</Button>
+                                </Col>
+                                <Col md={6} className="text-left ForumFlagged-removeModalCol">
+                                    <Button id="ForumFlagged-removeCancelBtn" onClick={this.handleRemoveCancel}>Cancel</Button>
+                                </Col>
+                            </Row>
+                        </Modal.Body>
+                    </Modal>: ''
+                }
+
+                {this.state.keepModal == true ? 
+                    <Modal show={this.state.keepModal} onHide={this.handleKeepModal} size="md" centered keyboard={false}>
+                        <Modal.Header closeButton></Modal.Header>
+                        <Modal.Body>
+                            <Row className="justify-content-center">
+                                <Col md={12} className="text-center ForumFlagged-keepModalCol">
+                                    <FontAwesomeIcon size="3x" icon={faCheckCircle}/>
+                                </Col>
+                            </Row>
+
+                            <Row className="justify-content-center">
+                                <Col md={12} className="text-center ForumFlagged-keepModalCol">
+                                    <h5 id="ForumFlagged-keepText">Do you want to keep this post?</h5>
+                                </Col>
+                            </Row>
+
+                            <Row className="justify-content-center">
+                                <Col md={6} className="text-right ForumFlagged-keepModalCol">
+                                    <Button id="ForumFlagged-keepConfirmBtn" onClick={this.handleKeepConfirm}>Confirm</Button>
+                                </Col>
+                                <Col md={6} className="text-left ForumFlagged-keepModalCol">
+                                    <Button id="ForumFlagged-keepCancelBtn" onClick={this.handleKeepCancel}>Cancel</Button>
+                                </Col>
+                            </Row>
+                        </Modal.Body>
+                    </Modal>: ''
+                }
+                
             </div>
         )
     }
