d3cf0cf0f63378133052153ca3fd355a5b663dde
beatricia <beatricia.inspirit@gmail.com>
2020-10-25 04:08:57 +0800

    Integrated ProgrammeTalkSchedule Table (Retrieve)

--- admin-openhouse/src/components/Marketing_Administrator/OpenHouseProgrammes/EditProgTalkModal.js
+++ admin-openhouse/src/components/Marketing_Administrator/OpenHouseProgrammes/EditProgTalkModal.js
@@ -244,3 +244,405 @@ export default class EditProgTalkModal extends React.Component {
     }
 }
 
+
+
+ {/* day1 */}
+        {/* <div>
+        {this.state.day1date &&
+                this.state.day1date.map((day1) => {
+                  return (
+                    <p>{day1}</p>
+                  )})}
+          <table id="users" class="table table-bordered"> 
+            <tbody>
+              <tr>
+                <th scope="col">S/N</th>
+                <th scope="col">Programme Talk</th>
+                <th scope="col">Awarding University</th>
+                <th scope="col">Start Time</th>
+                <th scope="col">End Time</th>
+                <th scope="col">Venue</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+              </tr>
+              {this.state.day1 &&
+                this.state.day1.map((day1,index) => {
+                  return (
+                    <tr>
+                        <td>{index+1}</td>
+                      <td>
+                      <span class={day1.docid + "text"}>
+                      {day1.talkName}
+                        </span>
+                          
+                          <span id={day1.docid + "spantalkname"} hidden>
+                          <input
+                            id={day1.docid + "talkname"}
+                            defaultValue={day1.talkName}
+                            type="text"
+                            name={day1.docid + "talkname"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day1.talkName}
+                            required
+                          />
+                        </span>            
+                      </td>
+                      <td>
+                      <span class={day1.docid + "text"}>
+                      {day1.awardingUni}
+                        </span>
+                          <span id={day1.docid + "spanawarduni"} hidden>
+                          <input
+                            id={day1.docid + "awarduni"}
+                            defaultValue={day1.awardingUni}
+                            type="text"
+                            name={day1.docid + "awarduni"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day1.awardingUni}
+                            required
+                          />
+                        </span>  
+                      </td>
+                      <td>
+                      <span class={day1.docid + "text"}>
+                      {day1.startTime}
+                        </span>
+                          <span id={day1.docid + "spanstarttime"} hidden>
+                          <input
+                            id={day1.docid + "starttime"}
+                            defaultValue={day1.startTime}
+                            type="text"
+                            name={day1.docid + "starttime"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day1.startTime}
+                            required
+                          />
+                        </span>  
+                      </td>
+                      <td>
+                      <span class={day1.docid + "text"}>
+                      {day1.endTime}
+                        </span>
+                          <span id={day1.docid + "spanendtime"} hidden>
+                          <input
+                            id={day1.docid + "endtime"}
+                            defaultValue={day1.endTime}
+                            type="text"
+                            name={day1.docid + "endtime"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day1.endTime}
+                            required
+                          />
+                        </span>  
+                      </td>
+                      <td>
+                      <span class={day1.docid + "text"}>
+                      {day1.venue}
+                        </span>
+                          <span id={day1.docid + "spanvenue"} hidden>
+                          <input
+                            id={day1.docid + "venue"}
+                            defaultValue={day1.venue}
+                            type="text"
+                            name={day1.docid + "venue"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day1.venue}
+                            required
+                          />
+                        </span>  
+                      </td>
+                      <td>
+                        <button
+                          id={day1.docid + "editbutton"}
+                          onClick={(e) => {
+                            this.editProgTalk(e, day1.docid);
+                          }}
+                        >
+                          Edit
+                        </button>
+
+                        <button
+                          id={day1.docid + "updatebutton"}
+                          hidden
+                          onClick={(e) => {
+                            this.update(e, day1.docid);
+                          }}
+                        >
+                          Update
+                        </button>
+                        <button
+                          hidden
+                          id={day1.docid + "cancelbutton"}
+                          onClick={(e) => {
+                            this.CancelEdit(e, day1.docid);
+                          }}
+                        >
+                          Cancel
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          onClick={(e) => {
+                            this.DeleteProgrammeTalk(e, day1.docid);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+        <div> */}
+
+        {/* day2 */}
+        {/* {this.state.day2date &&
+                this.state.day2date.map((day2) => {
+                  return (
+                    <p>{day2}</p>
+                  )})}
+          <table id="users" class="table table-bordered"> 
+            <tbody>
+              <tr>
+                <th scope="col">S/N</th>
+                <th scope="col">Programme Talk</th>
+                <th scope="col">Awarding University</th>
+                <th scope="col">Start Time</th>
+                <th scope="col">End Time</th>
+                <th scope="col">Venue</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+              </tr>
+              {this.state.day2 &&
+                this.state.day2.map((day2,index) => {
+                  return (
+                    <tr>
+                        <td>{index+1}</td>
+                      <td>
+                      <span class={day2.docid + "text"}>
+                      {day2.talkName}
+                        </span>
+                          
+                          <span id={day2.docid + "spantalkname"} hidden>
+                          <input
+                            id={day2.docid + "talkname"}
+                            defaultValue={day2.talkName}
+                            type="text"
+                            name={day2.docid + "talkname"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day2.talkName}
+                            required
+                          />
+                        </span>            
+                      </td>
+                      <td>
+                      <span class={day2.docid + "text"}>
+                      {day2.awardingUni}
+                        </span>
+                          <span id={day2.docid + "spanawarduni"} hidden>
+                          <input
+                            id={day2.docid + "awarduni"}
+                            defaultValue={day2.awardingUni}
+                            type="text"
+                            name={day2.docid + "awarduni"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day2.awardingUni}
+                            required
+                          />
+                        </span>  
+                      </td>
+                      <td>
+                      <span class={day2.docid + "text"}>
+                      {day2.startTime}
+                        </span>
+                          <span id={day2.docid + "spanstarttime"} hidden>
+                          <input
+                            id={day2.docid + "starttime"}
+                            defaultValue={day2.startTime}
+                            type="text"
+                            name={day2.docid + "starttime"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day2.startTime}
+                            required
+                          />
+                        </span>  
+                      </td>
+                      <td>
+                      <span class={day2.docid + "text"}>
+                      {day2.endTime}
+                        </span>
+                          <span id={day2.docid + "spanendtime"} hidden>
+                          <input
+                            id={day2.docid + "endtime"}
+                            defaultValue={day2.endTime}
+                            type="text"
+                            name={day2.docid + "endtime"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day2.endTime}
+                            required
+                          />
+                        </span>  
+                      </td>
+                      <td>
+                      <span class={day2.docid + "text"}>
+                      {day2.venue}
+                        </span>
+                          <span id={day2.docid + "spanvenue"} hidden>
+                          <input
+                            id={day2.docid + "venue"}
+                            defaultValue={day2.venue}
+                            type="text"
+                            name={day2.docid + "venue"}
+                            class="form-control"
+                            aria-describedby="emailHelp"
+                            placeholder={day2.venue}
+                            required
+                          />
+                        </span>  
+                      </td>
+                      <td>
+                        <button
+                          id={day2.docid + "editbutton"}
+                          onClick={(e) => {
+                            this.editProgTalk(e, day2.docid);
+                          }}
+                        >
+                          Edit
+                        </button>
+
+                        <button
+                          id={day2.docid + "updatebutton"}
+                          hidden
+                          onClick={(e) => {
+                            this.update(e, day2.docid);
+                          }}
+                        >
+                          Update
+                        </button>
+                        <button
+                          hidden
+                          id={day2.docid + "cancelbutton"}
+                          onClick={(e) => {
+                            this.CancelEdit(e, day2.docid);
+                          }}
+                        >
+                          Cancel
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          onClick={(e) => {
+                            this.DeleteProgrammeTalk(e, day2.docid);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+        <form onSubmit={this.addProgrammeTalks}>
+          <input
+            type="text"
+            name="talkName"
+            placeholder="Programme Talk"
+            onChange={this.updateInput}
+            value={this.state.talkName}
+            required
+          />
+          <input
+            type="text"
+            name="date"
+            placeholder="Date"
+            onChange={this.updateInput}
+            value={this.state.date}
+            required
+          />
+          <input
+            type="text"
+            name="startTime"
+            placeholder="Start Time"
+            onChange={this.updateInput}
+            value={this.state.startTime}
+            required
+          />
+          <input
+            type="text"
+            name="endTime"
+            placeholder="End Time"
+            onChange={this.updateInput}
+            value={this.state.endTime}
+            required
+          />
+          <input
+            type="text"
+            name="awardingUni"
+            placeholder="Awarding University"
+            onChange={this.updateInput}
+            value={this.state.awardingUni}
+            required
+          />
+          <input
+            type="text"
+            name="venue"
+            placeholder="Venue"
+            onChange={this.updateInput}
+            value={this.state.venue}
+            required
+          />
+
+          <input
+            type="text"
+            name="capacityLimit"
+            placeholder="Capacity Limit"
+            onChange={this.updateInput}
+            value={this.state.capacityLimit}
+            required
+          />
+         
+          <select id = "recordingvalue" required>
+            <option disabled selected value></option>
+            <option value="true">true</option>
+            <option value="false">false</option>
+          </select>
+
+          <select id = "livestatus" required>
+            <option disabled selected value></option>
+            <option value="true">true</option>
+            <option value="false">false</option>
+          </select>
+
+          <input
+            type="text"
+            name="noRegistered"
+            placeholder="No of Student Registered"
+            onChange={this.updateInput}
+            value={this.state.noRegistered}
+            required
+          />
+
+          <input
+            type="text"
+            name="link"
+            placeholder="Link"
+            onChange={this.updateInput}
+            value={this.state.link}
+            required
+          />
+          <button type="submit">Add Programme Talk</button>
+        </form> */}
+
