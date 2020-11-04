4a2a6eaf7fa77f6c32d5979d00362792edff80eb
beatricia <beatricia.inspirit@gmail.com>
2020-11-05 04:48:29 +0800

    GenerateAttendance UI + Integrated (Counter have issue)

--- admin-openhouse/src/pages/Marketing_Administrator/GenerateReport/GenerateAttendance.js
+++ admin-openhouse/src/pages/Marketing_Administrator/GenerateReport/GenerateAttendance.js
@@ -2,12 +2,11 @@ import React, { Component, useReducer } from "react";
 import fire from "../../../config/firebase";
 import history from "../../../config/history";
 import firecreate from "../../../config/firebasecreate";
-import { Container, Row, Col, Button, Table, Modal } from "react-bootstrap";
+import { Container, Row, Col, Button, Table, Form } from "react-bootstrap";
 
 import "../../../css/Marketing_Administrator/GenerateAttendance.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { faEdit, faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 
 import NavBar from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
@@ -29,9 +28,9 @@ class GenerateAttendance extends Component {
       id: "",
       totalNumber: 0,
       date: "",
-      useremail:"",
-      universityvalue:"",
-      programmenamevalue:""
+      userEmail:"",
+      universityValue:"",
+      programmeNameValue:""
     };
   }
 
@@ -43,7 +42,7 @@ class GenerateAttendance extends Component {
         var getrole = db
         .collection("Administrators")
         .where("email", "==", user.email);
-        this.state.useremail=user.email;
+        this.state.userEmail=user.email;
 
         getrole.get().then((snapshot) => {
           snapshot.forEach((doc) => {
@@ -139,19 +138,19 @@ display() {
 
   handleUniversityChange = (e) => {
     this.setState({
-      universityvalue: e.target.value,
+      universityValue: e.target.value,
     });
     this.setState({
-      universityvalue: e.target.value,
+      universityValue: e.target.value,
     },() => { this.universityFiltered() })
   }
 
   handleProgrammeChange = (e) => {
     this.setState({
-      programmenamevalue: e.target.value,
+      programmeNameValue: e.target.value,
     });
     this.setState({
-      universityvalue: e.target.value,
+      universityValue: e.target.value,
     },() => { this.programmenameFiltered() })
   }
 
@@ -161,17 +160,17 @@ display() {
     }
 
     const db = fire.firestore();
-    var getprogrammename="";
+    var getProgrammeName="";
     var counter = 1;
-    this.state.universityName = this.state.universityvalue;
+    this.state.universityName = this.state.universityValue;
 
     //Retrieve Attendance
     const userRef = db
-    .collection("Attendance").where("universityName","==",this.state.universityvalue)
+    .collection("Attendance").where("universityName","==",this.state.universityValue)
     .onSnapshot((snapshot) => {
       const attendance = [];
       snapshot.forEach((doc) => {
-        getprogrammename = doc.data().programmeName
+        getProgrammeName = doc.data().programmeName
         const data = {
           firstName: doc.data().firstName,
           lastName: doc.data().lastName,
@@ -187,14 +186,14 @@ display() {
         console.log(data)
       });
       this.setState({ 
-        programmeName : getprogrammename,
+        programmeName : getProgrammeName,
         attendance: attendance 
       });
     })
 
     const programmename = [];
     const programmeNamequery = db
-    .collection("Attendance").where("universityName","==",this.state.universityvalue)
+    .collection("Attendance").where("universityName","==",this.state.universityValue)
     .onSnapshot((snapshot) => {     
       snapshot.forEach((doc) => {
         programmename.push(doc.data().programmeName);
@@ -212,18 +211,18 @@ display() {
     }
 
     const db = fire.firestore();
-    var getprogrammename="";
-    var getuniversityname =""
+    var getProgrammeName="";
+    var getUniversityName =""
     var counter = 1;
     //Retrieve Attendance
 
     const userRef = db
-    .collection("Attendance").where("programmeName","==",this.state.programmenamevalue)
+    .collection("Attendance").where("programmeName","==",this.state.programmeNameValue)
     .onSnapshot((snapshot) => {
       const attendance = [];
       snapshot.forEach((doc) => {
-        getprogrammename = doc.data().programmeName
-        getuniversityname =doc.data().universityName
+        getProgrammeName = doc.data().programmeName
+        getUniversityName =doc.data().universityName
 
         const data = {
           firstName: doc.data().firstName,
@@ -240,24 +239,28 @@ display() {
         console.log(data)
       });
       this.setState({ 
-        universityName : getuniversityname,
-        programmeName : getprogrammename,
+        universityName : getUniversityName,
+        programmeName : getProgrammeName,
         attendance: attendance 
       });
     })
   }
 
   generateReport = () => {
-    console.log(this.state.attendance)
+    console.log("attendance: " + this.state.attendance)
     var id ="";
     if(this.state.attendance.length > 0){
       id = this.state.attendance[0].id;
     }
+
     const db = fire.firestore();
     var counter = 0;
     
     const total = db
-    .collection("Attendance").where("id","==",id)
+    .collection("Attendance")
+    .where("id","==",id)
+    // .where("universityName", "==", this.state.universityName)
+    // .where("programmeName", "==", this.state.programmeName)
     .onSnapshot((snapshot) => {
       counter = snapshot.size
       this.setState(
@@ -275,7 +278,7 @@ display() {
   }
 
   generatePDF(){
-    var doc = new jsPDF("landscape");
+    var doc = new jsPDF('p', 'mm', [297, 210]);
     var monthNames = [
       "January", "February", "March",
       "April", "May", "June", "July",
@@ -283,60 +286,179 @@ display() {
       "November", "December"
     ];
 
+    var user = this.state.userEmail;
+    var adminuser = "\nRequested by: " + user;
+    doc.setFontSize(11);   
+    doc.text(14, 25, adminuser);
+
     var today = new Date();    
     var day = today.getDate();
     var monthIndex = today.getMonth();
     var year = today.getFullYear();
     var date = day + ' ' + monthNames[monthIndex] + ' ' + year;
-    var newdat = "\nDate Requested : "+ date;
-    doc.setFontSize(14);   
-    doc.text(206,20,newdat);
+    var newdate = "\nDate Requested: "+ date;
+    doc.setFontSize(11);   
+    doc.text(132, 25, newdate);
 
-    var user = this.state.useremail;
-    var adminuser = "\nRequested by : " + user;
-    doc.setFontSize(14);   
-    doc.text(14,20,adminuser);
-
-    doc.line(14, 30, 283, 30);
+    // Line Separator
+    doc.setLineWidth(0.8);
+    doc.line(14, 35, 196, 35);
 
     var universityName = "\nUniversity Name: " + this.state.universityName;
-    doc.text(14,33,universityName); 
+    doc.setFontSize(12);
+    doc.text(14, 38, universityName); 
     
     var programmeName = "\nProgramme Name: " + this.state.programmeName;
-    doc.text(14,40,programmeName);
+    doc.text(14, 46, programmeName);
 
-    var totalNumber = "\nTotal Number : " + this.state.totalNumber;
-    doc.setFontSize(20);   
-    doc.text(215,34,totalNumber);
+    var totalNumber = "\nTotal Number: " + this.state.totalNumber; 
+    doc.text(158, 42, totalNumber);
 
-    doc.line(14, 50, 283, 50);
+    // Line Separator
+    doc.line(14, 55, 196, 55);
 
-    doc.setFontSize(12);
-    doc.text(14,57,"List of People Registered");
+    doc.setFontSize(11);
+    doc.text(14, 64, "List of Registrants");
 
     doc.autoTable({
       html: "#attendance",
-      startY: 63,
-      didParseCell: function (data) {
-        var rows = data.table.body;
-        if (data.row.index === 0) {
-          data.cell.styles.fontStyle = "bold";
-        }
+      headStyles: { 
+        halign: 'center', 
+        fillColor: [136, 183, 181] ,
+        font: 'helvetica',
+        cellPadding: {top: 2, right: 2, bottom: 2, left: 2},
       },
+      columnStyles: { 
+        0: { halign: 'center'},
+        1: { halign: 'center'},
+        2: { halign: 'center'},
+        3: { halign: 'center'},
+        4: { halign: 'center'},
+        5: { halign: 'left'},
+      },
+      startY: 70,
+      pageBreak: "auto"
     });
 
-    doc.setFontSize(18); 
-    doc.text("Report on Total Number of Attendees for Programme Talks", 65, 15);
+    doc.setFontSize(13); 
+    doc.setFont('helvetica', 'bold');
+    doc.text("Total Number of Attendees for Programme Talks Report", 50, 20);
       
-    doc.save("StudentAttendance.pdf");
+    doc.save("StudentAttendance_" + date +".pdf");
   };
 
 
   render() {
     return (
       <div>
-        
-        <label>
+        <Container fluid className="generateAttendanceCon">
+          <NavBar isMA={true} />
+
+          <Container fluid className="generateAttendanceContent">
+            <Row>
+              {/* SideNavBar Col */}
+              <Col md="2" style={{ paddingRight: "0" }} className="sideNavBarCol">
+                <SideNavBar />
+              </Col>
+
+              {/* Contents Col */}
+              <Col md="10" style={{ paddingLeft: "0" }}>
+                <Container fluid className="generateAttendanceContentCon">
+                  {/* Generate Attendance Page Header row */}
+                  <Row className="justify-content-center generateAttendanceContentHeaderRow">
+                    <Col md="9" className="text-left generateAttendanceContentHeaderCol">
+                      <h4 className="generateAttendanceHeaderText">Report on Total Number of Attendees for Programme Talks</h4>
+                    </Col>
+
+                    <Col md="3" className="text-right generateAttendanceContentHeaderCol">
+                      <Button className="generateReportBtn" onClick={this.generateReport}>
+                        <FontAwesomeIcon size="lg" className="generateReportBtnIcon" icon={faPlus} />
+                        <span className="generateReportBtnText">Generate Report</span>
+                      </Button>
+                    </Col>
+                  </Row>
+
+                  {/* Filter Row */}
+                  <Row className="justify-content-center generateAttendanceFilterRow">
+                    <Col md="6" className="generateAttendanceFilterCol">
+                      <Form.Label className="generateAttendanceFormLabel text-left">Choose University:</Form.Label>                                     
+                          
+                      <Form.Control as="select" name="universityName" defaultValue="" onChange={this.handleUniversityChange} className="generateAttendanceFormSelect text-center" required noValidate>
+                        <option value="" className="generateAttendanceFormSelectOption" disabled="disabled">Choose University</option>
+
+                        {this.state.university && this.state.university.map((uni) => {
+                          return (
+                            <>
+                              <option value={uni} className="generateAttendanceFormSelectOption">{uni}</option>
+                            </>
+                          );
+                        })}
+                      </Form.Control>
+                    </Col>
+                  
+                    <Col md="6" className="generateAttendanceFilterCol">
+                      <Form.Label className="generateAttendanceFormLabel text-left">Choose Programme:</Form.Label>                                     
+                          
+                      <Form.Control as="select" name="programmeName" defaultValue="" onChange={this.handleProgrammeChange} className="generateAttendanceFormSelect text-center" required noValidate>
+                        <option value="" className="generateAttendanceFormSelectOption" disabled="disabled">Choose Programme</option>
+
+                        {this.state.programmename && this.state.programmename.map((programmeName) => {
+                          return (
+                            <>
+                              <option value={programmeName} className="generateAttendanceFormSelectOption">{programmeName}</option>
+                            </>
+                          );
+                        })}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+
+                  {/* Table Row */}
+                  <Row className="justify-content-center generateAttendanceReportTableRow">
+                    <Col md="12" className="text-center">
+                      <Table responsive="sm" hover bordered id="attendance" className="generateAttendanceReportTable">
+                        <thead>
+                          <tr>
+                            <th className="generateAttendanceReportHeader_SNo">S/N</th>
+                            <th className="generateAttendanceReportHeader_Name">Name</th>
+                            <th className="generateAttendanceReportHeader_Email">Email</th>
+                            <th className="generateAttendanceReportHeader_Date">Attended Date</th>
+                            <th className="generateAttendanceReportHeader_Uni">University</th>
+                            <th className="generateAttendanceReportHeader_ProgName">Programme Name</th>
+                          </tr>
+                        </thead>
+
+                        <tbody>
+                          {this.state.attendance && this.state.attendance.map((attendance) => {
+                            return (
+                              <tr key={attendance.id}>
+                                <td className="generateAttendanceReportData_SNo text-center">{attendance.counter}</td>
+                                <td className="generateAttendanceReportData_Name text-center">{attendance.firstName} {attendance.lastName}</td>
+                                <td className="generateAttendanceReportData_Email text-center">{attendance.email}</td>
+                                <td className="generateAttendanceReportData_Date text-center">{attendance.date}</td>
+                                <td className="generateAttendanceReportData_Uni text-center">{attendance.universityName}</td>
+                                <td className="generateAttendanceReportData_ProgName text-left">{attendance.programmeName}</td>
+                              </tr>
+                            );
+                          })}
+                        </tbody>
+                      </Table>
+                    </Col>
+                  </Row>
+
+                </Container>
+
+              </Col>
+            </Row>
+          </Container>
+
+          <Footer />
+        </Container>
+
+
+
+
+        {/* <label>
           Choose a University:
           <select onChange={this.handleUniversityChange}>
           <option selected="true" disabled="disabled">Choose a University</option>    
@@ -362,10 +484,10 @@ display() {
                   );
                 })}          
           </select>
-        </label>
+        </label> */}
 
         {/* Do not change the id below*/}
-        <div>
+        {/* <div>
           <table id="attendance" class="table table-bordered">
             <tbody>
               <tr>
@@ -396,7 +518,7 @@ display() {
             </tbody>
           </table>
         </div>
-        <button onClick={this.generateReport}>Generate PDF</button>
+        <button onClick={this.generateReport}>Generate PDF</button> */}
       </div>
     );
   }
