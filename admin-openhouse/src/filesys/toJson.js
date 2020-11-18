41bb25bc9971bb8bdf3d07b8e570b64496317045
syteo <shin.shiying@gmail.com>
2020-11-18 23:56:54 +0800

    file conversions

---
+++ admin-openhouse/src/filesys/toJson.js
@@ -0,0 +1,27 @@
+const db = require('./config/adminConfig');
+const fs = require('fs');
+const converter = require('json-2-csv');
+
+const pathDir = `./${process.argv.slice(2)[0]}/`;
+const args = process.argv.slice(2)[1];
+
+const csvFile = fs.readFileSync(`${pathDir}${args}.csv`, 'utf-8', err => {
+    if (err) return console.log(`${err} Error occured`);
+});
+
+converter.csv2json(csvFile, (err, json) => {
+    if (err) return console.log(`${err} Error occured`);
+    console.log(json);
+
+    fs.writeFileSync(`${pathDir}${args}Converted.json`, JSON.stringify(json, null, 2), err => {
+        if (err) return console.log(`${err} Error writing file`)
+    });
+
+    // To store in Firestore
+    /* json.forEach(elem => {
+        db.collection('toJSONTest').doc(elem.id).set(elem).catch(err => {
+            return console.log(err);
+        })
+    }); */
+});
+    
